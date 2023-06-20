d5970a3dce12a9fc74abcf5d28b98c7a49afceef
JustinZarb <21224625+JustinZarb@users.noreply.github.com>
2023-06-20 16:54:44 +0200

    added prompts

---
+++ pages/benchmarking.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import os
+import pandas as pd
+
+st.title("Benchmarks")
+st.markdown(
+    "Check [Github](https://github.com/JustinZarb/shade-calculator/tree/main/dev/prompts)"
+)
